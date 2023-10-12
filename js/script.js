040f2d1bc6786e7b1459215173ea7714746dd081
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-12 21:28:40 +0200

    first for

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-let mybox = document.getElementById('my-box');
+let myBox = document.getElementById('my-box');
 // console.log(mybox);
+
+for(let x = 1; x <101; x++) {
+
+    let div = document.createElement("div");
+
+    if (x % 3 === 0 && x % 5 === 0) {
+        // console.log('FizzBuzz');
+        div.classList.add('my-small-box');
+        div.classList.add('my-bg-orange');
+        div.innerText = `FizzBuzz`;
+        myBox.append(div);
+    } else if(x % 3 === 0){
+        // console.log('fizz');
+    }
+}
